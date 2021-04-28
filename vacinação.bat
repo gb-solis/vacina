fbc43ee4d9ff490daa477582b8a73d98b2dbf5d0
gb-solis <74569045+gb-solis@users.noreply.github.com>
2021-04-27 22:26:52 -0300

    Corrigi erro, adicionei suporte a caracteres especiais

--- vacinação.bat
+++ vacinação.bat
@@ -1,2 +1,3 @@
+chcp 1252
 call C:\Users\<SEU USUARIO AQUI>\Anaconda3\Scripts\activate.bat
-"C:\Users\<SEU USUARIO AQUI>\Anaconda3\python.exe" ""
+"C:\Users\<SEU USUARIO AQUI>\Anaconda3\python.exe" "vacinação.py"
