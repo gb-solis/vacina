eef68730c210ccaa48dcfadc38536af8363a9c72
Leonardo Almeida Lessa <leonardo.a.lessa@gmail.com>
2021-05-22 20:01:55 -0300

    Adicionei linhas de melhor estimativa no plot sob opção -l

--- plot.py
+++ plot.py
@@ -1,4 +1,4 @@
-from vacinação import perfis, load_dados
+from vacinação import perfis, load_dados, melhor_estimativa
 from matplotlib import pyplot as plt
 from matplotlib import dates as mdates
 import matplotlib.units as munits
@@ -9,28 +9,35 @@ import datetime
 converter = mdates.ConciseDateConverter(show_offset=False)
 munits.registry[datetime.date] = converter
 
-def plota(pessoas):
+def plota(pessoas, linhas = False):
     for nome in pessoas:
         x, y = load_dados(nome)
-        plt.plot_date(x, y, 'o:', xdate=True)
+        p = plt.plot_date(x, y, 'o:', xdate=True, label = str(nome))
+        estim = melhor_estimativa(nome)
+        if linhas:
+            plt.axline((mdates.date2num(x[0]), (estim - x[0]).days), slope=-1, color = p[0].get_color(), linestyle = '--', label = estim)
         
     plt.title('Tempo estimado para a vacinação')
     plt.xlabel('Data de acesso')
     plt.ylabel('Dias estimados até a vacinação')
-    plt.legend(pessoas)
+    plt.legend()
     plt.show()
 
 if __name__ == "__main__":
-    ajudamsg = 'Uso: plot.py pessoa1 pessoa2 ... \nou   plot.py -p [--perfis] (para ver perfis) \nou   plot.py -h [--help] (para ver esta ajuda)'
+    ajudamsg = 'Uso: plot.py [-l] pessoa1 pessoa2... (-l para linhas de estimativa) \nou   plot.py -p [--perfis] (para ver perfis) \nou   plot.py -h [--help] (para ver esta ajuda)'
     pessoas = sys.argv[1:]
     nomes = [perfil['name'] for perfil in perfis]
+    linhas = False
     if pessoas == ['-h'] or pessoas == ['-help']:
         print(ajudamsg)
     elif pessoas == ['-p'] or pessoas == ['-perfis']:
         print('Os perfis em perfis.json são:')
         map(print, nomes)
     else: 
+        if pessoas[0] == '-l':
+            linhas = True
+            del(pessoas[0])
         for pessoa in pessoas:
             if pessoa not in nomes:
                 raise Exception(f'Pessoa {pessoa} não é um dos perfis de perfis.json')
-        plota(pessoas)
+        plota(pessoas, linhas)
